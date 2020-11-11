d376d9c712994e2ea3962eab5e9c2d805c0a61d6
Nadia Nicoll Aguilar Flores <naguilar@HN-LT17130m.loe.corp>
2020-11-10 22:46:32 -0600

    Added graphics

--- src/components/Wedding.vue
+++ src/components/Wedding.vue
@@ -2,26 +2,26 @@
   <div class="home" id="home">
     <nav class="navbar navbar-expand-lg">
       <img class="navbar-brand" src="../assets/nd-logo_1.svg">
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
+      <button class="navbar-toggler menu-mobile">
+        <span class="navbar-toggler-icon"></span> 
       </button>
       <div class="collapse navbar-collapse" id="navbarResponsive">
         <ul class="navbar-nav ml-auto">
           <li class="nav-item active">
-            <a class="nav-link" href="#">NUESTRO SÍ
+            <a class="nav-link" href="#Nuestrosi">NUESTRO SÍ
               <span class="sr-only">(current)</span>
             </a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="#">TE INVITAMOS</a>
+            <a class="nav-link" href="#TeInvitamos">TE INVITAMOS</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="#">CEREMONIA VIRTUAL</a>
+            <a class="nav-link" href="#Ceremonia">CEREMONIA VIRTUAL</a>
           </li>
         </ul>
       </div>
     </nav>
-    <div class=" estas-invitado">
+    <div class=" estas-invitado" id="Nuestrosi">
        <div class="container">
          <div class="block-center">
            <img class="bg-header" src="../assets/nadia-dante.svg">
@@ -30,22 +30,35 @@
     </div>
     <div class="nuestro-si ">
       <div class="container">
-        <img class="img-inv" src="../assets/invitado.svg">
-        <div class="text-container mb-5">
+        <img class="img-inv" src="../assets/invitado.svg" id="TeInvitamos">
+        <div class="text-container">
           <p>Estamos felices de compartir con ustedes nuestro enlace matrimonial, ya que han formado parte importante en el proceso de este evento; por ello presentamos la invitación a la ceremonia de amor y eternidad que confirma nuestra unión.</p>
         </div>
+        <div class="block-inv mb-5">
+          <img src="../assets/bg-wedd-card.png">
+        </div>
       </div>
+     
     </div>
-    <div class="invitacion-img"></div>
-    <div class="ceremonia">
+    <!-- <div class="invitacion-img"></div> -->
+    <div class="ceremonia bg-top-left" id="Ceremonia">
       <div class="container">
-        <img class="img-inv-ceremonia" src="../assets/ceremonia.svg">
-        <div class="text-container text-dark mb-5">
+        <img class="img-inv" src="../assets/ceremonia.svg">
+        <div class="text-container text-dark ">
           <p>El enlace a la transmisión via zoom será habilitado a las 11:00 am del 14 de Noviembre del 2020</p>
         </div>
-        <button class="button-ceremonia" type="button">Enlace de la Ceremonia</button>
+        <button class="button-ceremonia mb-5" type="button">Enlace de la Ceremonia</button>
       </div>
     </div>
+    <footer class="bg-bottom-right">
+      <div class="container">
+        <div class="block-center mt-3 mb-3">
+          <img class="logo-footer" src="../assets/nd-logo_1.svg">
+          <p class="text-center">Tegucigalpa, Honduras</p>
+          <p class="text-center"> © 2020</p>
+        </div>
+      </div>
+    </footer>
   </div>
 </template>
 
